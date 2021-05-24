ee968bf6086b126a21ae480c7c4aa62e8a9e2bfc
edo Lorenza <edolrnz@gmail.com>
2021-05-24 14:33:19 +0700

    add appicon, setup : launcsreen storyboard, update: completion handler in authmanager

--- Spotify/Manager/AuthManager.swift
+++ Spotify/Manager/AuthManager.swift
@@ -123,14 +123,14 @@ final class AuthManager {
         }
     }
     
-    public func refreshIfNeeded(completion: @escaping (Bool) -> Void ) {
+    public func refreshIfNeeded(completion: ((Bool) -> Void)?) {
         guard !refreshingToken else {
             return
             
         }
         
         guard shouldRefreshToken else {
-            completion(true)
+            completion?(true)
             return
         }
         guard let refreshToken = self.refreshToken else { return }
@@ -160,7 +160,7 @@ final class AuthManager {
         let data = basicToken.data(using: .utf8)
         guard let base64String = data?.base64EncodedString() else {
             print("Failure to get base64")
-            completion(false)
+            completion?(false)
             return
         }
 
@@ -171,7 +171,7 @@ final class AuthManager {
             self?.refreshingToken = false
             guard let data = data,
                   error == nil else {
-                completion(false)
+                completion?(false)
                 return
             }
 
@@ -180,11 +180,11 @@ final class AuthManager {
                 self?.onRefreshBlock.forEach{ $0(result.access_token)}
                 self?.onRefreshBlock.removeAll()
                 self?.cacheToken(result: result)
-                completion(true)
+                completion?(true)
             }
             catch {
                 print(error.localizedDescription)
-                completion(false)
+                completion?(false)
             }
         }
         task.resume()
--- Spotify/Resources/SceneDelegate.swift
+++ Spotify/Resources/SceneDelegate.swift
@@ -19,6 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         if AuthManager.shared.isSignedIn {
+            AuthManager.shared.refreshIfNeeded(completion: nil)
             window?.rootViewController = TabBarViewController()
         }else {
             let navVC = UINavigationController(rootViewController: WelcomeViewController())
