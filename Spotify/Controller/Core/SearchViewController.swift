4faad38ce82d77b04d684904b49e90bee8d8f8b3
edo Lorenza <edolrnz@gmail.com>
2021-05-27 14:48:18 +0700

    setup :  search bar controller UI

--- Spotify/Controller/Core/SearchViewController.swift
+++ Spotify/Controller/Core/SearchViewController.swift
@@ -7,11 +7,41 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UISearchResultsUpdating {
+  
+    //MARK: - Properties
+    let searchController: UISearchController = {
+        let result = SearchResultViewController()
+        let vc = UISearchController(searchResultsController: result)
+        vc.searchBar.placeholder = "Songs, Artist, Albums"
+        vc.searchBar.searchBarStyle = .minimal
+        vc.definesPresentationContext = true
+        return vc
+    }()
+    
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        setupView()
     }
     
+    //MARK: - API
+    
+    //MARK: - Helpers
+    private func setupView(){
+        view.backgroundColor = .systemBackground
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
+    }
+    
+}
+
+extension SearchViewController {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let resultController = searchController.searchResultsController as? SearchResultViewController ,let query = searchController.searchBar.text, !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        //ferform search
+        print(query)
+    }
 }
--- Spotify/Controller/SearchResultViewController.swift
+++ Spotify/Controller/SearchResultViewController.swift
@@ -8,22 +8,17 @@
 import UIKit
 
 class SearchResultViewController: UIViewController {
-
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Helpers
+    private func setupView(){
+        
     }
-    */
-
 }
